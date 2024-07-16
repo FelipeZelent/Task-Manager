ff7411a91c594ab8896b0145bb4f6fead6a42acb
FelipeZelent <felipezelent@gmail.com>
2024-07-16 14:20:13 -0300

    v1

---
+++ src/main/java/com/taskmanager/taskmanager/TaskManagementeApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class TaskManagementeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementeApplication.class, args);
+    }
+}
---
+++ src/main/java/com/taskmanager/taskmanager/controller/TaskController.java
@@ -0,0 +1,57 @@
+package com.taskmanager.taskmanager.controller;
+
+import com.taskmanager.taskmanager.model.Task;
+import com.taskmanager.taskmanager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTask() {
+        return taskService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Optional<Task> task = taskService.findById(id);
+        return task.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task savedTask = taskService.save(task);
+        return new ResponseEntity<>(savedTask, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Optional<Task> optionalTask = taskService.findById(id);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            task.setTitle(taskDetails.getTitle());
+            task.setDescription(taskDetails.getDescription());
+            task.setCompleted(taskDetails.isCompleted());
+            Task updateTask = taskService.save(task);
+            return ResponseEntity.ok(updateTask);
+        } else {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
